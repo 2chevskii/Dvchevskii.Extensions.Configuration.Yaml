7fc0a4f5f22135cdc8026b4e40c22817bc41eb66
2CHEVSKII <prsroman3@gmail.com>
2023-05-15 11:41:02 +0300

    Trying to fix absolute paths

--- src/Dvchevskii.Extensions.Configuration.Yaml.FileExtensions/YamlFileConfigurationSource.cs
+++ src/Dvchevskii.Extensions.Configuration.Yaml.FileExtensions/YamlFileConfigurationSource.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.FileProviders;
 using YamlDotNet.Serialization;
+using SPath = System.IO.Path;
 
 namespace Dvchevskii.Extensions.Configuration.Yaml.FileExtensions
 {
@@ -11,6 +13,20 @@ namespace Dvchevskii.Extensions.Configuration.Yaml.FileExtensions
 
         public override IConfigurationProvider Build(IConfigurationBuilder builder)
         {
+            if ( SPath.IsPathRooted(Path) )
+            {
+                string directory = SPath.GetDirectoryName(Path);
+                Path         = SPath.GetFileName(Path);
+                FileProvider = new PhysicalFileProvider(directory);
+            }
+            else
+            {
+                string fullPath  = SPath.GetFullPath(Path);
+                string directory = SPath.GetDirectoryName(fullPath);
+                FileProvider = new PhysicalFileProvider(directory);
+                Path         = SPath.GetFileName(Path);
+            }
+
             EnsureDefaults(builder);
             return new YamlFileConfigurationProvider(this);
         }
